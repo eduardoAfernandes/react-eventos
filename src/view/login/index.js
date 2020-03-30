ea12355e384a099515c08e6cabebb78d57669ee5
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2020-03-30 11:11:37 -0300

    Autenticao de Usuario com Firebase

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import './login.css'
+import React,{ useState } from 'react';
+import './login.css';
+import firebase from '../../config/firebase';
+import 'firebase/auth';
 
 function Login() {
+
+    const [email,setEmail] = useState();
+    const [senha,setSenha] = useState();
+
+    function logar() {
+        // alert("Logar");
+        firebase.auth().signInWithEmailAndPassword(email, senha).then(resultado =>{
+            alert("Usuario Logado");
+        }).catch(erro => {
+            alert(erro);
+        });
+    }
+
     return (
         <div className="login-content d-flex align-items-center">
             <form className="form-signin mx-auto">
                 <div className="text-center mb-4">
-                    <img className="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
                     <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">Login</h1>
                 </div>
 
-                <input type="email" id="inputEmail" className="form-control my-2" placeholder="Email" />
-                <input type="password" id="inputPassword" className="form-control my-2" placeholder="Senha" />
+                <input onChange={(e) => setEmail(e.target.value) } type="email" id="inputEmail" className="form-control my-2" placeholder="Email" autoComplete />
+                <input onChange={(e) => setSenha(e.target.value) } type="password" id="inputPassword" className="form-control my-2" placeholder="Senha" autoComplete />
 
-                <button className="btn btn-lg btn-login btn-block" type="submit">Entrar</button>
+                <button onClick={logar} className="btn btn-lg btn-login btn-block" type="button">Logar</button>
 
             <div className="msg-login text-white text-center my-5">
                 <span><strong>WOW!</strong> Voce está conectado! &#128526;</span> <br></br>
